4f55c9b71c6ebf2910cd3d48f182fdcb403533a9
fluffyorang3 <fluffyorang3@gmail.com>
2024-06-23 10:27:01 +0200

    Complete main page functionality

--- buttonController.js
+++ buttonController.js
@@ -1,9 +1,11 @@
-const newProjectButton = document.getElementById('project-button')
 const newTaskButton = document.getElementById('task-button')
+const newProjectButton = document.getElementById('project-button')
 const taskDialog = document.getElementById('task-dialog')
 const projectDialog = document.getElementById('project-dialog')
 const taskSubmit = document.getElementById('task-submit')
 const projectSubmit = document.getElementById('project-submit')
+const closeButtonTask = document.querySelector('#task-close')
+const closeButtonProject = document.querySelector('#project-close')
 
 newTaskButton.addEventListener("click", () => {
     taskDialog.showModal();
@@ -11,4 +13,12 @@ newTaskButton.addEventListener("click", () => {
 
 newProjectButton.addEventListener("click", () => {
     projectDialog.showModal();
+})
+
+closeButtonTask.addEventListener("click", () => {
+    taskDialog.close()
+})
+
+closeButtonProject.addEventListener("click", () => {
+    projectDialog.close()
 })
